91f8e8d15d990bf3b8409dbe1a01bd0d24210bc0
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2025-07-25 18:53:22 -0300

    fix: corrige nomes das URLs

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.get_users, name='get_all_users'),
-    path('user/<str:nickname>/', views.get_user_by_nickname, name='get_user_by_nickname'),
+    path('users/', views.get_users, name='get_all_users'),
+    path('users/<str:nickname>/', views.get_user_by_nickname, name='get_user_by_nickname'),
 ]
